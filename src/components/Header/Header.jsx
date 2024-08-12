c72a5dc896cc877d357450715de49e7ec69866c5
Alexandr Moroz <log.koyd@gmail.com>
2024-08-12 20:35:59 +0300

    update for phone 2, size scale

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,8 +4,10 @@ import styles from "./styles.module.css";
 export const Header = () => {
     return (
         <header className={styles.header}>
-            <h1 className={styles.title}>MAP News </h1>
-            <p className={styles.date}>{formatDate(new Date())}</p>
+            <div className={styles.centers}>
+                <h1 className={styles.title}>MAP News </h1>
+                <p className={styles.date}>{formatDate(new Date())}</p>
+            </div>
         </header>
     )
 }
--- src/components/Image/Image.jsx
+++ src/components/Image/Image.jsx
@@ -3,9 +3,7 @@ import styles from "./styles.module.css";
 export const Image = ({image}) => {
     return (
         <div className={styles.wrapper}>
-            {image ?
-                <img src={image} alt="Ups, no image" className={styles.image}/>
-                : null}
+                {image ? <img src={image} alt="news" className={styles.image} /> : null}
         </div>
     )
 }
